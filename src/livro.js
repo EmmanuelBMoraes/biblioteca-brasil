d172bd5185c501126b2786248912e9cd73e9bb30
Emmanuel-Moraes <emmanuelbmoraes@gmail.com>
2025-11-29 19:13:02 -0300

    feat: implementar logica do livro

--- src/livro.js
+++ src/livro.js
@@ -1,1 +1,21 @@
-module.exports = class Livro {};
+module.exports = class Livro {
+  constructor(titulo, autor, anoPublicacao, genero, isbn, disponivel) {
+    if (!isbn) {
+      throw new Error("ISBN é obrigatório");
+    }
+    this.titulo = titulo;
+    this.autor = autor;
+    this.anoPublicacao = anoPublicacao;
+    this.genero = genero;
+    this.isbn = isbn;
+    this.disponivel = disponivel;
+  }
+
+  disponibilizar() {
+    this.disponivel = true;
+  }
+
+  indisponibilizar() {
+    this.disponivel = false;
+  }
+};
